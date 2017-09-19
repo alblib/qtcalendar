e46af388afca9c5fe36b4980efe416ac9d3819a6
Albertus Liberius Humaniensis Disciplinus <w890702@gmail.com>
2017-09-19 17:06:16 +0900

    qcalendar

--- qtCalendar/WCalendar.cpp
+++ qtCalendar/WCalendar.cpp
@@ -1,34 +1,91 @@
 #include "WCalendar.h"
+#include <exception>
+#include <stdexcept>
 
 WCalendar::WCalendar()
 {
 
 }
 
-QDate Holidays::WKoreanHolidays::Seolnal(int year)
+WCalendarNamespace::WDatePeriod::WDatePeriod(const QDate &_start, const QDate &_end):
+    t_start(_start), t_end(_end)
+{
+    if (t_start != QDate() && t_end != QDate() && t_start > t_end) throw std::exception();
+}
+
+QDate WCalendarNamespace::WDatePeriod::start() const
+{
+    return t_start;
+}
+
+QDate WCalendarNamespace::WDatePeriod::end() const
+{
+    return t_end;
+}
+
+bool WCalendarNamespace::WDatePeriod::in(const QDate &op) const
+{
+    return (t_start <= op && op <= t_end);
+}
+
+WCalendarNamespace::WAbstractAnniversary::WAbstractAnniversary()
+{
+
+}
+
+WCalendarNamespace::WDatePeriod WCalendarNamespace::WAbstractAnniversary::getDefinedRange() const
+{
+    return foundation;
+}
+
+void WCalendarNamespace::WAbstractAnniversary::setDefinedRange(const WCalendarNamespace::WDatePeriod &foun)
+{
+    foundation = foun;
+}
+
+WCalendarNamespace::WGregoryAnniversary::WGregoryAnniversary(int startyear, int endyear, int month, int mday)
+{
+    t_month = month;
+    t_mday = mday;
+    setDefinedRange(WDatePeriod(QDate(startyear,1,1),QDate(endyear,12,31)));
+}
+
+QDate WCalendarNamespace::WGregoryAnniversary::indexed(int year) const
+{
+    return QDate(year,t_month,t_mday);
+}
+
+QList<QDate> WCalendarNamespace::WGregoryAnniversary::ranged(const WCalendarNamespace::WDatePeriod &range) const
+{
+    QList<QDate> result;
+    int year = range.start().year();
+    if (QDate(year,t_month,t_mday) < range.start()) year++;
+    for (;temp <= range.end(); temp.addYears(1));
+    return result;
+}
+
+QDate WCalendarNamespace::WKoreanHolidays::Seolnal(int year)
 {
     if (year == 2006) return QDate(2006,1,29);
     if (year == 2007) return QDate(2007,2,18);
-    if (year == 2008) return QDate(2008,2,07);
+    if (year == 2008) return QDate(2008,2, 7);
     if (year == 2009) return QDate(2009,1,26);
     if (year == 2010) return QDate(2010,2,14);
-    if (year == 2011) return QDate(2011,2,03);
+    if (year == 2011) return QDate(2011,2, 3);
     if (year == 2012) return QDate(2012,1,23);
     if (year == 2013) return QDate(2013,2,10);
     if (year == 2014) return QDate(2014,1,31);
     if (year == 2015) return QDate(2015,2,19);
-    if (year == 2016) return QDate(2016,2,08);
+    if (year == 2016) return QDate(2016,2, 8);
     if (year == 2017) return QDate(2017,1,28);
     if (year == 2018) return QDate(2018,2,16);
-    if (year == 2019) return QDate(2019,2,05);
+    if (year == 2019) return QDate(2019,2, 5);
     if (year == 2020) return QDate(2020,1,25);
     if (year == 2021) return QDate(2021,2,12);
-    if (year == 2022) return QDate(2022,2,01);
+    if (year == 2022) return QDate(2022,2, 1);
     if (year == 2023) return QDate(2023,1,22);
     if (year == 2024) return QDate(2024,2,10);
     if (year == 2025) return QDate(2025,1,29);
     if (year == 2026) return QDate(2026,2,17);
-    if (year == 2027) return QDate(2027,2,07);
+    if (year == 2027) return QDate(2027,2, 7);
 }
-
-
--- qtCalendar/WCalendar.h
+++ qtCalendar/WCalendar.h
@@ -2,6 +2,7 @@
 #define WCALENDAR_H
 
 #include <QDate>
+#include <QList>
 
 class WCalendar
 {
@@ -11,22 +12,45 @@ public:
 
 
 namespace WCalendarNamespace{
-    struct WDatePeriod{
-        QDate start,end;
-        WDatePeriod(const QDate& _start = QDate(), const QDate& _end = QDate()):
-            start(_start), end(_end)
-        {
-            if (start == QDate())
-        }
-
+    class WDatePeriod{
+    private:
+        QDate t_start, t_end;
+    public:
+        WDatePeriod(const QDate& _start = QDate(), const QDate& _end = QDate());
+        QDate start() const;
+        QDate end() const;
+        bool in(const QDate&) const;
     };
 
+    class WAbstractAnniversary{
+    private:
+        WDatePeriod foundation;
+    protected:
+        WAbstractAnniversary();
+    public:
+        WDatePeriod getDefinedRange() const;
+        void setDefinedRange(const WDatePeriod& foun);
+
+        virtual QDate indexed(int year) const = 0;
+        virtual QList<QDate> ranged(const WDatePeriod& range) const = 0;
+    };
+
+    class WGregoryAnniversary: public WAbstractAnniversary{
+    private:
+        int t_month, t_mday;
+    public:
+        WGregoryAnniversary(int startyear, int endyear, int month, int mday);
+        virtual QDate indexed(int year) const;
+        virtual QList<QDate> ranged(const WDatePeriod& range) const;
+    };
+
+    /*
     struct WKoreanJapanese{
         static constexpr QDate start = QDate(1910,10,1); //언제시작?총독부?
         static constexpr QDate end = QDate(1945,9,2);
 
     };
-
+*/
     struct WKoreanHolidays{
         static QDate Seolnal(int year);
         static QDate Chuseok(int year);
